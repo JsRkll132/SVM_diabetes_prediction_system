ab988cef62c92e23a38a52e8652f26759774222a
JsRkll132 <jose.apaza2@unmsm.edu.pe>
2024-12-02 14:59:46 -0500

    front 100%

--- ml-app-view/src/components/predict_page.js
+++ ml-app-view/src/components/predict_page.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 const PredictPage = () => {
   const [formData, setFormData] = useState({
@@ -13,7 +15,6 @@ const PredictPage = () => {
     Age: "",
   });
 
-  const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
 
   const handleChange = (e) => {
@@ -27,22 +28,37 @@ const PredictPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Convertir los valores a números
       const numericData = Object.keys(formData).reduce((acc, key) => {
         acc[key] = parseFloat(formData[key]);
         return acc;
       }, {});
 
       const response = await axios.post("http://localhost:5000/predict", numericData);
-      setPrediction(response.data.prediction);
+
+      const prediction = response.data.prediction;
+
+      Swal.fire({
+        title: "Resultado de la Predicción",
+        text:
+          prediction === "diabetic"
+            ? "El paciente es diabético."
+            : "El paciente no es diabético.",
+        icon: prediction === "diabetic" ? "warning" : "info",
+        confirmButtonText: "Aceptar",
+        confirmButtonClass: "btn btn-success",
+        buttonsStyling: true, // Habilita estilos personalizados con Bootstrap
+        customClass: {
+          confirmButton: prediction === "diabetic" ? "btn btn-danger" : "btn btn-success",
+          popup: "border border-secondary rounded-3 shadow", // Estilo de borde Bootstrap
+        },
+      });
+
       setError(null);
     } catch (err) {
       setError(err.response?.data?.error || "Ocurrió un error");
-      setPrediction(null);
     }
   };
 
-  // Traducción de las etiquetas para el frontend
   const fieldLabels = {
     Pregnancies: "Número de embarazos",
     Glucose: "Glucosa en sangre (mg/dL)",
@@ -54,15 +70,14 @@ const PredictPage = () => {
     Age: "Edad (años)",
   };
 
-  // Rango de valores válidos según la tabla
   const inputRanges = {
     Pregnancies: { min: 0, max: 17 },
-    Glucose: { min: 0, max: 500 },
+    Glucose: { min: 65, max: 500 },
     BloodPressure: { min: 0, max: 122 },
     SkinThickness: { min: 0, max: 99 },
     Insulin: { min: 0, max: 846 },
     BMI: { min: 14, max: 67.1 },
-    DiabetesPedigreeFunction: { min: 0.078, max: 2.42, step: 0.01 },
+    DiabetesPedigreeFunction: { min: 0.078, max: 2.420, step: 0.01 },
     Age: { min: 21, max: 81 },
   };
 
@@ -73,7 +88,16 @@ const PredictPage = () => {
         <div className="row g-3 justify-content-center">
           {Object.keys(formData).map((field) => (
             <div key={field} className="col-md-6">
-              <label className="form-label">{fieldLabels[field]}:</label>
+              <label className="form-label d-flex justify-content-between">
+                <span>{fieldLabels[field]}:</span>
+                {field !== "Pregnancies" && (
+                  <span className="text-muted">
+                    <strong>
+                      ({inputRanges[field].min} - {inputRanges[field].max})
+                    </strong>
+                  </span>
+                )}
+              </label>
               <input
                 type="number"
                 step={inputRanges[field].step || "any"}
@@ -95,11 +119,6 @@ const PredictPage = () => {
         </div>
       </form>
 
-      {prediction && (
-        <div className="alert alert-success mt-4 text-center">
-          <h4>Predicción: {prediction}</h4>
-        </div>
-      )}
       {error && (
         <div className="alert alert-danger mt-4 text-center">
           <h4>Error: {error}</h4>
